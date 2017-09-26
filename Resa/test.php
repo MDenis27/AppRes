168fbbf3802e5458a638849b125ddfddbc986691
Maxime Denis <maxime.denis@hotmail.com>
2017-09-26 15:54:12 +0200

    initial commit

---
+++ Resa/test.php
@@ -0,0 +1,57 @@
+<head>
+<link rel="stylesheet" type="text/css" href="test.css">
+<title>Reservation</title>
+<h1> Reservation </h1>
+</head>
+
+<form>
+  <div style="
+            position: absolute;
+            top: 5em;
+   "> <h3>Destination:</h3><br>
+  </div>
+  <div style="
+            position: absolute;
+            top: 8em;
+   "> <h3>Quantity:</h3><br>
+  </div>
+
+  <div style="
+            position: absolute;
+            top: 5em;
+            left: 15em;
+  ">
+  <ul id="menu">
+   <li><a href="#">Lien menu 1</a>
+      <ul>
+         <li><a href="#">lien sous menu 1</a></li>
+         <li><a href="#">lien sous menu 1</a></li>
+         <li><a href="#">lien sous menu 1</a></li>
+         <li><a href="#">lien sous menu 1</a></li>
+      </ul>
+   </li>
+   </ul>
+   </div>
+   <div style="
+            position: absolute;
+            top: 8em;
+            left: 15em;
+  "><h3></h3>
+  <input type="text" name="quantity">
+  </div>
+  </div>
+  <div style="
+            position: absolute;
+            top: 11em;
+   "> <h3>Cancellation insurance:</h3><br>
+  </div>
+  <div style="
+            position: absolute;
+            top: 11em;
+            left: 15em;
+  "><h3></h3>
+  <input id="checkBox" type="checkbox">
+  </div>
+</form>
+
+<<button class='button' >Solution</button>
