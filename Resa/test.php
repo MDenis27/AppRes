d1b8d360e179982ee2372519d2c3a3a390f008c3
Maxime Denis <maxime.denis@hotmail.com>
2017-09-26 18:03:54 +0200

    1st page done

--- Resa/test.php
+++ Resa/test.php
@@ -4,23 +4,48 @@
     <h1> Reservation </h1>
 </head>
 
-<h3> Please fill in the form below </h3>
+<h3>
+    Please fill in the form below
+</h3>
 
-<form>
-  Destination:
-  <select name="destination">
-    <option value="EHAM">Amsterdam</option>
-    <option value="LEBL">Barcelone</option>
-    <option value="EDDB">Berlin</option>
-    <option value="LFPG">Paris Charles De Gaulle</option>
-  </select>
-  <br>
-  Number of passagers:
-  <input type="text" name="number">
-  <br>
-  Cancellation insurance:
-  <input id="checkBox" type="checkbox" >
-  <br>
-  <input type="submit" value="Next step">
-
-</form>
+The price is 10€ until 12 years and then 15€.
+<br>
+The price of the cancellation insurance is 20€ regardless of the number of passengers.
+<br>
+<div style="
+    position absolute;
+    top 10em;
+">
+    <form>
+        <div style ="
+            position: absolute;
+        ">
+            Destination:
+            <br>
+            Number of passengers:
+            <br>
+            Cancellation insurance:
+        </div>
+        <div style="
+            position: absolute;
+            left: 15em;
+        ">
+            <select name="destination">
+                <option value="EHAM">Amsterdam</option>
+                <option value="LEBL">Barcelone</option>
+                <option value="EDDB">Berlin</option>
+                <option value="LFPG">Paris Charles De Gaulle</option>
+            </select>
+            <br>
+            <input type="text" name="number">
+            <br>
+            <input id="checkBox" type="checkbox" >
+        </div>
+        <div style="
+            position: absolute;
+            top: 15em;
+        ">
+        <input type="submit" value="Next step">
+        </div>
+    </form>
+</div>
