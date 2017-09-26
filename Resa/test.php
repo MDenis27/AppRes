f579d6547bbf7aa8d02ff320d249f09632324cc6
Maxime Denis <maxime.denis@hotmail.com>
2017-09-26 17:28:12 +0200

    rewrote the code properly

--- Resa/test.php
+++ Resa/test.php
@@ -1,65 +1,26 @@
 <head>
-<link rel="stylesheet" type="text/css" href="test.css">
-<title>Reservation</title>
-<h1> Reservation </h1>
+    <link rel="stylesheet" type="text/css" href="test.css">
+    <title>Reservation</title>
+    <h1> Reservation </h1>
 </head>
 
 <h3> Please fill in the form below </h3>
 
 <form>
-  <div style="
-            position: absolute;
-            top: 5em;
-   "> <h3>Destination:</h3><br>
-  </div>
+  Destination:
+  <select name="destination">
+    <option value="EHAM">Amsterdam</option>
+    <option value="LEBL">Barcelone</option>
+    <option value="EDDB">Berlin</option>
+    <option value="LFPG">Paris Charles De Gaulle</option>
+  </select>
+  <br>
+  Number of passagers:
+  <input type="text" name="number">
+  <br>
+  Cancellation insurance:
+  <input id="checkBox" type="checkbox" >
+  <br>
+  <input type="submit" value="Next step">
 
-  <div style="
-            position: absolute;
-            top: 8em;
-   "> <h3>Quantity:</h3><br>
-  </div>
-
-  <div style="
-            position: absolute;
-            top: 11em;
-   "> <h3>Cancellation insurance:</h3><br>
-  </div>
-
-
-
-   <div style="
-            position: absolute;
-            top: 8em;
-            left: 15em;
-  "><h3></h3>
-  <input type="text" name="quantity">
-  </div>
-
-  <div style="
-            position: absolute;
-            top: 11em;
-            left: 15em;
-  "><h3></h3>
-  <input id="checkBox" type="checkbox">
-  </div>
-
-    <div style="
-            position: absolute;
-            top: 5em;
-            left: 15em;
-  ">
-  <select>
-  <option value="volvo">Volvo</option>
-  <option value="saab">Saab</option>
-  <option value="mercedes">Mercedes</option>
-  <option value="audi">Audi</option>
-</select>
-   </div>
-
-  <div style="
-            position: absolute;
-            top: 14em;
-  "><h3></h3>
-  <input type="submit">
-  </div>
 </form>
