4331ee0a00ded9937fd92861d33fcecfc95e1d0a
Maxime Denis <maxime.denis@hotmail.com>
2017-09-26 17:09:05 +0200

    Correction of the drop down menu

--- Resa/test.php
+++ Resa/test.php
@@ -4,12 +4,15 @@
 <h1> Reservation </h1>
 </head>
 
+<h3> Please fill in the form below </h3>
+
 <form>
   <div style="
             position: absolute;
             top: 5em;
    "> <h3>Destination:</h3><br>
   </div>
+
   <div style="
             position: absolute;
             top: 8em;
@@ -18,20 +21,12 @@
 
   <div style="
             position: absolute;
-            top: 5em;
-            left: 15em;
-  ">
-  <ul id="menu">
-   <li><a href="#">Lien menu 1</a>
-      <ul>
-         <li><a href="#">lien sous menu 1</a></li>
-         <li><a href="#">lien sous menu 1</a></li>
-         <li><a href="#">lien sous menu 1</a></li>
-         <li><a href="#">lien sous menu 1</a></li>
-      </ul>
-   </li>
-   </ul>
-   </div>
+            top: 11em;
+   "> <h3>Cancellation insurance:</h3><br>
+  </div>
+
+
+
    <div style="
             position: absolute;
             top: 8em;
@@ -39,12 +34,7 @@
   "><h3></h3>
   <input type="text" name="quantity">
   </div>
-  </div>
-  <div style="
-            position: absolute;
-            top: 11em;
-   "> <h3>Cancellation insurance:</h3><br>
-  </div>
+
   <div style="
             position: absolute;
             top: 11em;
@@ -52,6 +42,24 @@
   "><h3></h3>
   <input id="checkBox" type="checkbox">
   </div>
-</form>
 
-<<button class='button' >Solution</button>
+    <div style="
+            position: absolute;
+            top: 5em;
+            left: 15em;
+  ">
+  <select>
+  <option value="volvo">Volvo</option>
+  <option value="saab">Saab</option>
+  <option value="mercedes">Mercedes</option>
+  <option value="audi">Audi</option>
+</select>
+   </div>
+
+  <div style="
+            position: absolute;
+            top: 14em;
+  "><h3></h3>
+  <input type="submit">
+  </div>
+</form>
